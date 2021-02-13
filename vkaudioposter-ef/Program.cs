552238d21c22e51f501196baec17c958a6300553
Rhiskey <first3395@gmail.com>
2021-02-13 04:14:49 +0300

    init

---
+++ vkaudioposter-ef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vkaudioposter_ef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

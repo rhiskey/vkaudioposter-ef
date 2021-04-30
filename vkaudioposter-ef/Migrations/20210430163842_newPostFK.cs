d45412e96e38fd721ba249be2edfb6f947e81408
Vladimir Kiselev <first3395@gmail.com>
2021-04-30 19:48:08 +0300

    add foreign key to postedTracks

---
+++ vkaudioposter-ef/Migrations/20210430163842_newPostFK.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vkaudioposter_ef.Migrations
+{
+    public partial class newPostFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Playlists",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 4, 30, 19, 38, 42, 315, DateTimeKind.Local).AddTicks(2118),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 4, 30, 17, 51, 55, 983, DateTimeKind.Local).AddTicks(6541),
+                oldComment: "Update Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "console_Photostocks",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 4, 30, 19, 38, 42, 320, DateTimeKind.Local).AddTicks(6381),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 4, 30, 17, 51, 55, 990, DateTimeKind.Local).AddTicks(3515),
+                oldComment: "Update Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Playlists",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 4, 30, 17, 51, 55, 983, DateTimeKind.Local).AddTicks(6541),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 4, 30, 19, 38, 42, 315, DateTimeKind.Local).AddTicks(2118),
+                oldComment: "Update Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "console_Photostocks",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 4, 30, 17, 51, 55, 990, DateTimeKind.Local).AddTicks(3515),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 4, 30, 19, 38, 42, 320, DateTimeKind.Local).AddTicks(6381),
+                oldComment: "Update Date");
+        }
+    }
+}

e095cc6658efea8f5c588b0a813ab8ab8c8270e8
Vladimir Kiselev <35545041+rhiskey@users.noreply.github.com>
2021-05-27 01:47:07 +0300

    Keyless attribute

---
+++ vkaudioposter-ef/Migrations/20210526224647_AddKeylessTrackUrl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vkaudioposter_ef.Migrations
+{
+    public partial class AddKeylessTrackUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Playlists",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 5, 27, 1, 46, 47, 252, DateTimeKind.Local).AddTicks(8271),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 5, 27, 1, 37, 21, 753, DateTimeKind.Local).AddTicks(157),
+                oldComment: "Update Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "console_Photostocks",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 5, 27, 1, 46, 47, 254, DateTimeKind.Local).AddTicks(4045),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 5, 27, 1, 37, 21, 754, DateTimeKind.Local).AddTicks(5742),
+                oldComment: "Update Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Playlists",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 5, 27, 1, 37, 21, 753, DateTimeKind.Local).AddTicks(157),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 5, 27, 1, 46, 47, 252, DateTimeKind.Local).AddTicks(8271),
+                oldComment: "Update Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdateDate",
+                table: "console_Photostocks",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 5, 27, 1, 37, 21, 754, DateTimeKind.Local).AddTicks(5742),
+                comment: "Update Date",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 5, 27, 1, 46, 47, 254, DateTimeKind.Local).AddTicks(4045),
+                oldComment: "Update Date");
+        }
+    }
+}
--- vkaudioposter-ef/Model/TrackUrl.cs
+++ vkaudioposter-ef/Model/TrackUrl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace vkaudioposter_ef.Model
 {
+    [Keyless]
     public partial class TrackUrl
     {
         public string Key { get; set; }

563f048529fda0895a601a0eaee7ee72fe1c8343
rhiskey <first3395@gmail.com>
2023-08-12 16:08:36 +0300

    .net7

--- vkaudioposter-ef/CreateInitialSchema.cs
+++ vkaudioposter-ef/CreateInitialSchema.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
+﻿using System;
 
 namespace vkaudioposter_ef
 {
@@ -7,30 +6,30 @@ namespace vkaudioposter_ef
     {
         public static void CreateSchema(bool isFirstLaunch)
         {
-            string db = Program.db;
-            string qDrop = $"DROP DATABASE `{db}` ;";
-            string qCreate = $"CREATE SCHEMA `{db}` ;";
-            SqlConnection conn = new();
-            conn.ConnectionString = Program.connStr;
-            SqlCommand cmd = new();
-
-            try
-            {
-                conn.Open();
-                cmd.Connection = conn;
-                if (isFirstLaunch)
-                {
-                    cmd.CommandText = qDrop;
-                    cmd.ExecuteNonQuery();
-                }
-                cmd.CommandText = qCreate;
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error " + ex.StackTrace + " has occurred: " + ex.Message);
-            }
-            conn.Close();
+            // string db = Program.db;
+            // string qDrop = $"DROP DATABASE `{db}` ;";
+            // string qCreate = $"CREATE SCHEMA `{db}` ;";
+            // // SqlConnection conn = new();
+            // conn.ConnectionString = Program.connStr;
+            // SqlCommand cmd = new();
+            //
+            // try
+            // {
+            //     conn.Open();
+            //     cmd.Connection = conn;
+            //     if (isFirstLaunch)
+            //     {
+            //         cmd.CommandText = qDrop;
+            //         cmd.ExecuteNonQuery();
+            //     }
+            //     cmd.CommandText = qCreate;
+            //     cmd.ExecuteNonQuery();
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine("Error " + ex.StackTrace + " has occurred: " + ex.Message);
+            // }
+            // conn.Close();
 
         }
     }
